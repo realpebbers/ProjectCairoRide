7aa2a1bd420b4e45b39920fdf9264aaa08abfd9c
Aliibrahim3600 <aliibrahim3600@gmail.com>
2025-03-20 14:59:53 +0200

    Add Functions of subscribtion

--- Subscription_system.h
+++ Subscription_system.h
@@ -1,19 +1,45 @@
 #pragma once
 #include<iostream>
 #include<string>
+
+int const Max_Users = 1e5;
 using namespace std;
+
 struct Date
 {
 	int Day, Month, Year;
 };
+
+struct Subscription_Type 
+{
+	int Scholar_Pass,
+		General_Pass,
+		Smart_Wallet;
+};
+
 struct Subscription
 {
 	int  Remaining_Trips;
-	string Plan_Type;
+	Subscription_Type Plan_Type;
 	Date  Activation_Date, Expiry_Date;
 
-};
+}Data_Of_Users[Max_Users];
+
+
+void Scholar_Pass()
+{
+
+
+}
+void Genral_Pass()
+{
 
 
 
- 
+}
+void Smart_Wallet()
+{
+
+
+
+}
