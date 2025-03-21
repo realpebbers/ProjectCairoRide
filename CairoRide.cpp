858474b802356f3aab200bb479426b2c742ff83a
AbdallhaTaha0 <AT010Abdallha@gmail.com>
2025-03-22 00:19:18 +0200

    Fixed one main fun.

--- CairoRide.cpp
+++ CairoRide.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-<<<<<<< HEAD
+#include  "Individual_Ride.h"
 #include<string>
 #include"Subscription_system.h"
 
@@ -10,14 +10,6 @@ int main()
 {
     Subcribtion_Menu Menu;
     Menu.DisplayMenu();
-   
-    
-=======
-#include  "Individual_Ride.h"
-using namespace std;
-int main()
-{
 
->>>>>>> ea626efbd31978f2030c80caffb8b6fd22a86ea7
-    return 0;
 }
+
